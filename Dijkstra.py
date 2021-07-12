6824b97a584214a47e200fe029f942f020d99fa2
Snitzelberg <48810985+Snitzelberg@users.noreply.github.com>
2021-07-12 00:28:13 -0300

    Busca em Largura, Djikstra e Arquivo de teste
    Busca em Largura, Djikstra e Arquivo de teste

---
+++ Dijkstra.py
@@ -0,0 +1,49 @@
+from biblioteca_grafo import Grafo
+
+inf = 99999
+
+def Dijkstra(grafo,vertice_origem):
+    
+    if vertice_origem < 0 or vertice_origem >= grafo.qtdVertices():
+        return "Impossivel"
+    else:
+        distancias = {vertice: inf for vertice in range(grafo.qtdVertices())}
+        caminhos = {vertice: -1 for vertice in range(grafo.qtdVertices())}
+        distancias[vertice_origem] = 0
+        vertices = list(map(int,grafo.vertices.copy()))
+
+        while len(vertices) != 0:
+            menor_custo = min(vertices,key=lambda vertice: distancias[vertice])
+            
+            if distancias[menor_custo] == inf:
+                break
+            else:
+                vizinhos = []
+                for vert in grafo.vizinhos(menor_custo):
+                    if vert in vertices:
+                        vizinhos.append(vert)
+                
+                for vertice in vizinhos:
+                    melhor_caminho = distancias[menor_custo] + grafo.peso(menor_custo,vertice)
+                    if melhor_caminho < distancias[vertice]:
+                        distancias[vertice] = melhor_caminho
+                        caminhos[vertice] = menor_custo
+                
+                vertices.remove(menor_custo)
+        
+        for proximo, origem in caminhos.items():
+            caminho = []
+            
+            while origem >= 0:
+                caminho.insert(0,origem)
+                origem = caminhos[origem]
+            
+            caminho.append(proximo)
+            caminho = [str(string+1) for string in caminho]
+
+            print("%d: "%(proximo),end=" ")
+            print(",".join(caminho),end="")
+            print("; d=%.2f"%distancias[proximo])
+         
+grafo1 = Grafo("teste.txt")
+Dijkstra(grafo1,1)
---
+++ busca_largura.py
@@ -0,0 +1,33 @@
+from biblioteca_grafo import Grafo
+
+def busca_largura(grafo, vertice):
+    if vertice < 0 or vertice >= grafo.qtdVertices():
+        print("Impossivel")
+        return 0
+    
+    else:
+        visitados = [False] * (grafo.qtdVertices()+1)
+        fila = []
+        fila.append(vertice)
+        distancia = 0
+        
+        while len(fila) != 0:
+                nivel_atual = fila.copy()
+                fila = []
+                for vert in nivel_atual:
+                    v = vert
+                    visitados[v] = True
+                    vizinhos_false = list(filter(lambda x : visitados[x] == False, grafo.vizinhos(v)))
+
+                    for i in range(len(vizinhos_false)): 
+                        visitados[vizinhos_false[i]] = True
+                    fila.extend(vizinhos_false)
+                
+                nivel_atual = map(lambda x: str(x),nivel_atual)
+                print("%d:" % (distancia), end = ' ')
+                print(",".join(nivel_atual))
+                
+                distancia += 1
+
+grafo1 = Grafo("dolphins.txt")
+busca_largura(grafo1,200)
