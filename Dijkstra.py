2af7637f38f03adbd02a9b0dd9739f90373f9247
Snitzelberg <48810985+Snitzelberg@users.noreply.github.com>
2021-07-14 01:14:07 -0300

    Ultimos Ajustes

--- Dijkstra.py
+++ Dijkstra.py
@@ -3,18 +3,18 @@ from biblioteca_grafo import Grafo
 inf = 99999
 
 def Dijkstra(grafo,vertice_origem):
-    
     if vertice_origem < 0 or vertice_origem >= grafo.qtdVertices():
         return "Impossivel"
     else:
-        distancias = {vertice: inf for vertice in range(grafo.qtdVertices())}
-        caminhos = {vertice: -1 for vertice in range(grafo.qtdVertices())}
+        distancias = {vertice: inf for vertice in range(grafo.qtdVertices()+1)}
+        caminhos = {vertice: None for vertice in range(grafo.qtdVertices()+1)}
         distancias[vertice_origem] = 0
         vertices = list(map(int,grafo.vertices.copy()))
 
-        while len(vertices) != 0:
+
+        while (len(vertices)) != 0:
             menor_custo = min(vertices,key=lambda vertice: distancias[vertice])
-            
+
             if distancias[menor_custo] == inf:
                 break
             else:
@@ -22,7 +22,7 @@ def Dijkstra(grafo,vertice_origem):
                 for vert in grafo.vizinhos(menor_custo):
                     if vert in vertices:
                         vizinhos.append(vert)
-                
+
                 for vertice in vizinhos:
                     melhor_caminho = distancias[menor_custo] + grafo.peso(menor_custo,vertice)
                     if melhor_caminho < distancias[vertice]:
@@ -33,17 +33,18 @@ def Dijkstra(grafo,vertice_origem):
         
         for proximo, origem in caminhos.items():
             caminho = []
-            
-            while origem >= 0:
+            if not proximo:
+                continue  
+            while origem != None:
                 caminho.insert(0,origem)
                 origem = caminhos[origem]
-            
+                
             caminho.append(proximo)
-            caminho = [str(string+1) for string in caminho]
+            caminho = [str(string) for string in caminho]
 
             print("%d: "%(proximo),end=" ")
             print(",".join(caminho),end="")
             print("; d=%.2f"%distancias[proximo])
-         
-grafo1 = Grafo("teste.txt")
+
+grafo1 = Grafo("dolphins.txt")
 Dijkstra(grafo1,1)
