1135c59c98c32cea37534b3bd18539e41c003d6e
luizotaviosantosreis <luizotaviosantosreis@gmail.com>
2021-07-24 19:24:04 -0300

    Procurando erro

--- floyd-warshall.py
+++ floyd-warshall.py
@@ -1,14 +1,21 @@
 from biblioteca_grafo import Grafo
 
 def resultado(grafo):
-    print(grafo.pesos)
+    #for i in range(grafo.qtdVertices()):
+        #print("%d: " % (i + 1)) + ",".join(map(str, grafo.pesos[(i,i)])))
+    #print(grafo.pesos[(1,2)])
+    for i in range(grafo.qtdVertices()):
+        print("Peso: %.1f" % grafo.peso(1,i+1))
+        print("Pesos: %.1f" % grafo.pesos[(1,i+1)])
+        print("\n")
+
     print("\nRepresenta uma aresta de peso que não existe, ou seja, infinito")
 
 def floydwarshall(grafo):
-    for a in range (grafo.qtdVertices()):
-        for b in range (grafo.qtdVertices()):
-            for c in range (grafo.qtdVertices()):
-                grafo.pesos[(b,c)] = min(grafo.peso(b,c), grafo.peso(b,a) + grafo.peso(a,c))
+    for k in range (grafo.qtdVertices()):
+        for i in range (grafo.qtdVertices()):
+            for j in range (grafo.qtdVertices()):
+                grafo.pesos[(i,j)] = min(grafo.peso(i,j), grafo.peso(i,k) + grafo.peso(k,j))
     return grafo
 
 if __name__ == "__main__":
